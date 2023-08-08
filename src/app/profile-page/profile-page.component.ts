a9364c0bb763ad00a2a2fc9fb6b051d24238d48e
Adrienne Hui <adrienne.hui@ibm.com>
2023-08-08 14:05:41 -0600

    Adding profile page

---
+++ src/app/profile-page/profile-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss']
+})
+export class ProfilePageComponent {
+  profileData: any;
+  constructor(
+    private http: HttpClient) {
+    this.profileData = this.getProfileDetails();
+  }
+
+  public getProfileDetails() {
+    this.http.get<any>('https://mocki.io/v1/611a3036-4420-48a5-b8da-9b461853cdd2').subscribe(res => {
+      console.log(res);
+      this.profileData = res;
+    });
+  }
+}
